8c86ffb2ec7f62ac076358a9a42a25ed495ab0a0
Supraja Narasimhan <suprajanarasimhan@vmware.com>
2023-02-21 10:58:46 -0800

    [#184207986] Test if "job", "origin" and "source_id" can be customized
    - For custom app metrics sent via Metrics Registrar, see if "job", "origin" and "source_id" tag set in the app appear in Aria Apps.
    - Result: They do not appear in Aria Apps.

--- src/main/java/org/cloudfoundry/samples/music/web/AlbumController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/AlbumController.java
@@ -1,5 +1,7 @@
 package org.cloudfoundry.samples.music.web;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.cloudfoundry.samples.music.domain.Album;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +10,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Random;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping(value = "/albums")
@@ -15,11 +19,28 @@ public class AlbumController {
     private static final Logger logger = LoggerFactory.getLogger(AlbumController.class);
     private CrudRepository<Album, String> repository;
 
+
+    // Guess: As the constructor, it has the same name as the class itself: "AlbumController."
+    // It creates "AlbumController" objects.
     @Autowired
-    public AlbumController(CrudRepository<Album, String> repository) {
+    public AlbumController(CrudRepository<Album, String> repository, MeterRegistry registry) {
         this.repository = repository;
+        Gauge.builder("albumcontroller.randomvalue",fetchRandomValue()).
+                tag("version","v1").
+                tag("origin","albumorigin").
+                tag("job","albumjob").
+                tag("source_id","albumsource").
+                tag("album_tag","freshcuts").
+                description("albumcontroller descrip").
+                register(registry);
     }
 
+    // A Supplier<Number> reminds me of a reference to a function.
+    // Guess: The Gauge.builder() function takes Supplier, which it invokes using a get() at a different point in time.
+    public Supplier<Number> fetchRandomValue() {
+         Supplier<Number> randomFloatSupplier = () -> new Random().nextFloat();
+         return randomFloatSupplier;
+    }
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<Album> albums() {
         return repository.findAll();
